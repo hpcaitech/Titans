a97b81d87c4108415c95c98d645543001378380c
FrankLeeeee <somerlee.9@gmail.com>
2022-05-05 13:56:34 +0800

    [import] improved import scope

---
+++ titans/layer/__init__.py
@@ -0,0 +1,9 @@
+from . import attention
+from . import batchnorm
+from . import block
+from . import embedding
+from . import head
+from . import mlp
+from .init_rules import init_rules
+
+__all__ = ['attention', 'batchnorm', 'block', 'embedding', 'head', 'mlp', 'init_rules']
