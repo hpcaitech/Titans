c7ee0823397d128a97f7b1123d59e1f5a15b3327
FrankLeeeee <somerlee.9@gmail.com>
2022-05-09 16:14:20 +0800

    [unittest] reuse test code and added rerun decorator

--- tests/test_layer/test_block/test_gpt_block.py
+++ tests/test_layer/test_block/test_gpt_block.py
@@ -1,15 +1,12 @@
 import colossalai
 import pytest
 import torch
-import torch.multiprocessing as mp
 import torch.nn.functional as F
 
 from titans.layer.block import GPTBlock
 from titans.utils import split_data_for_tensor_parallel
-from colossalai.utils import free_port
-from colossalai.nn.layer.utils import divide
-from colossalai import nn as col_nn
-from functools import partial
+from colossalai.testing import rerun_if_address_is_in_use
+from tests.utils import run_with_parallel_config
 from colossalai.global_variables import tensor_parallel_env as tp_env
 
 BATCH_SIZE = 4
@@ -42,14 +39,6 @@ def run_dist(rank, world_size, port, config):
 
 
 @pytest.mark.parametrize('parallel_config', [(4, '1d'), (4, '2d'), (4, '2.5d'), (8, '2.5d'), (8, '3d')])
+@rerun_if_address_is_in_use()
 def test_gpt_block(parallel_config):
-    world_size, tp_mode = parallel_config
-    port = free_port()
-
-    config = dict(parallel=dict(tensor=dict(size=world_size, mode=tp_mode)))
-
-    if tp_mode == '2.5d':
-        config['parallel']['tensor']['depth'] = world_size // 4
-
-    run_func = partial(run_dist, world_size=world_size, port=port, config=config)
-    mp.spawn(run_func, nprocs=world_size)
+    run_with_parallel_config(*parallel_config, run_func=run_dist)
---
+++ tests/utils/dist_test.py
@@ -0,0 +1,24 @@
+import torch.multiprocessing as mp
+from colossalai.utils import free_port
+from functools import partial
+
+
+def run_with_parallel_config(world_size, parallel_mode, run_func):
+    """
+    A wrapper function to reuse the same code snippet in layer/model testing.
+
+    Args:
+        world_size (int): the number of processes to launch
+        parallel_mode (str): the parallelism method used
+        run_func (Callable): the function to launch multiple processes, must have world_size, port and config as arguments.
+    """
+
+    port = free_port()
+
+    config = dict(parallel=dict(tensor=dict(size=world_size, mode=parallel_mode)))
+
+    if parallel_mode == '2.5d':
+        config['parallel']['tensor']['depth'] = world_size // 4
+
+    run_func = partial(run_func, world_size=world_size, port=port, config=config)
+    mp.spawn(run_func, nprocs=world_size)
