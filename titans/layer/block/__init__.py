47bc973f71fcf7cc89cfa6972986715f5e8da73e
FrankLeeeee <somerlee.9@gmail.com>
2022-05-09 15:55:36 +0800

    [model] removed preact resnet and simclr

--- titans/layer/block/__init__.py
+++ titans/layer/block/__init__.py
@@ -1,5 +1,4 @@
 from .gpt_block import GPTBlock, MOEGPTBlock
 from .vit_block import ViTBlock
-from .resnet_block import PreActBlock, PreActBottleneck
 from .transformer_encoder import TransformerEncoderLayer, TransformerEncoder
 from .transformer_decoder import TransformerDecoderLayer, TransformerDecoder
