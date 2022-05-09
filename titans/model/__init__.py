a654d107ea3e090d3d172eb92fc22dbb0b2f3cf8
Frank Lee <somerlee.9@gmail.com>
2022-05-09 16:23:49 +0800

    [model] removed preact resnet and simclr (#23)

--- titans/model/__init__.py
+++ titans/model/__init__.py
@@ -2,9 +2,8 @@ from . import detr
 from . import gpt
 from . import knowledge_graph_embedding
 from . import moe
-from . import simclr
 from . import transformer
 from . import vilt
 from . import vit
 
-__all__ = ['detr', 'gpt', 'knowledge_graph_embedding', 'moe', 'simclr', 'ransformer', 'vilt', 'vit']
+__all__ = ['detr', 'gpt', 'knowledge_graph_embedding', 'moe', 'transformer', 'vilt', 'vit']
