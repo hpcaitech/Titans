e597e6d19c572df83aac8442a3fd480cf9cfa8c4
Frank Lee <somerlee.9@gmail.com>
2022-05-09 14:59:35 +0800

    [layer] merged gpt and transformer mlp (#21)
    * [layer] merged gpt and transformer mlp
    
    * [layer] merged gpt and transformer mlp

--- titans/layer/block/__init__.py
+++ titans/layer/block/__init__.py
@@ -3,6 +3,3 @@ from .vit_block import ViTBlock
 from .resnet_block import PreActBlock, PreActBottleneck
 from .transformer_encoder import TransformerEncoderLayer, TransformerEncoder
 from .transformer_decoder import TransformerDecoderLayer, TransformerDecoder
-from .gpt_1d_layer import GPTTransformerLayer1D, FusedGPTTransformerLayer1D
-from .transformer_1d import GenericTransformerLayer1D
-
--- titans/layer/mlp/transformer_mlp.py
+++ titans/layer/mlp/transformer_mlp.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from colossalai import nn as col_nn
 from typing import Callable
 from torch import Tensor
+from torch import dtype
 
 
 class TransformerMLP(nn.Module):
@@ -15,7 +16,8 @@ class TransformerMLP(nn.Module):
         mlp_ratio (int): the multiplication factor of the linear dimension, default is 4.
         act_func (Callable): the activation function, default is None which will use GeLU.
         dropout_prob (float): the probability of dropout, default is 0.
-        eps (float): the epsilon of layernorm, default is 1e-05.
+        dtype (torch.dtype): the data type for model parameters, default is None.
+        bias (bool): whether the linear layers have bias, default is True.
     """
 
     def __init__(self,
@@ -23,12 +25,14 @@ class TransformerMLP(nn.Module):
                  mlp_ratio: int = 4,
                  act_func: Callable = None,
                  dropout_prob: float = 0.0,
-                 eps: float = 1e-05):
+                 dtype: dtype = None,
+                 bias: bool = True):
         super().__init__()
+        intermediate_dim = int(hidden_size * mlp_ratio)
 
         # int linear layers
-        self.linear_1 = col_nn.Linear(hidden_size, int(hidden_size * mlp_ratio))
-        self.linear_2 = col_nn.Linear(int(hidden_size * mlp_ratio), hidden_size)
+        self.linear_1 = col_nn.Linear(hidden_size, intermediate_dim, dtype=dtype, bias=bias)
+        self.linear_2 = col_nn.Linear(intermediate_dim, hidden_size, dtype=dtype, bias=bias)
 
         # int activation function
         if act_func:
@@ -42,8 +46,6 @@ class TransformerMLP(nn.Module):
         else:
             self.dropout = None
 
-        self.layernorm = col_nn.LayerNorm(normalized_shape=hidden_size, eps=eps)
-
     def forward(self, x: Tensor) -> Tensor:
         intermediate_activate = self.linear_1(x)
         intermediate_activate = self.act_func(intermediate_activate)
@@ -52,7 +54,5 @@ class TransformerMLP(nn.Module):
 
         if self.dropout:
             output = self.dropout(output)
-
-        output = self.layernorm(output + x)
 
         return output
